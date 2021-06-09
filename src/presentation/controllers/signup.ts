f2adee1a7758fe5998b26070ba8e0ec4c502eab8
Israel Dantas <israeldantasleite@gmail.com>
2021-06-08 21:41:48 -0300

    refactor: move duplicated code to a helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,18 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../erros/missing-param-error'
+import { badRequest } from '../helpers/http-helpers'
 export class SignUpController {
-  handle (httpRequest): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    }
+    if (!httpRequest.body.name) {
+      return badRequest(new MissingParamError('name'))
+    }
     return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+      statusCode: 200,
+      body: 'success'
     }
   }
 }
